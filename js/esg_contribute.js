c55b607be524a2b8f9c51a78154ba15d4a65f3ce
hoon95 <gmline_@naver.com>
2023-07-31 22:49:01 +0900

    [File] esg script 파일 추가

---
+++ js/esg_contribute.js
@@ -0,0 +1,20 @@
+$('article').bxSlider({
+    pager: false,
+    auto: true,
+    controls: false,
+    minSlides: 2,
+    maxSlides: 2,
+    moveSlides: 1,
+    slideMargin: 100,
+    slideWidth: 1280,
+    pause: 4000,
+    speed: 4000
+});
+
+$('.ctr_sub').bxSlider({
+    pager: false,
+    controls: false,
+    mode: 'vertical',
+    randomStart: true,
+    infiniteLoop: false
+});
