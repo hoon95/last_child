4ef411e826cd80ba3c4236fccc219c4acb7eefa7
hoon95 <gmline_@naver.com>
2023-07-31 16:37:29 +0900

    [File] esg 사회공헌 html, css, script 추가

---
+++ js/esg_contribute.js
@@ -0,0 +1,12 @@
+$('article').bxSlider({
+    pager: false,
+    auto: true,
+    controls: false,
+    minSlides: 2,
+    maxSlides: 2,
+    moveSlides: 1,
+    slideMargin: 100,
+    slideWidth: 1280,
+    pause: 2000,
+    speed: 2000
+});
