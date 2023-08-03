d702d6938b63ac43c0a7c154b2ba5f6eceee0254
hoon95 <gmline_@naver.com>
2023-08-03 21:46:15 +0900

    [Add] swiper js 추가

--- js/esg_contribute.js
+++ js/esg_contribute.js
@@ -6,8 +6,20 @@ let ctrSlide = $('article').bxSlider({
     tickerHover: true,
     speed: 50000
   });
+// let SwiperTicker = new Swiper('.ctr_main', {
+//   spaceBetween: 0,
+//   centeredSlides: true,
+//   speed: 6000,
+//   autoplay: {
+//     delay: 1,
+//   },
+//   loop: true,
+//   slidesPerView:'auto',
+//   allowTouchMove: false,
+//   disableOnInteraction: true
+// });
 
-
-AOS.init();
-
-pushInStart();
+let swiper = new Swiper('.mySwiper', {
+  effect: "cards",
+  grabCursor: true,
+});
