63161ec2d1713bb23d240a3c02d96d6b8d836974
hoon95 <gmline_@naver.com>
2023-08-03 21:47:18 +0900

    [Fix] 텍스트 스크롤 수정

--- js/esg_intro.js
+++ js/esg_intro.js
@@ -1,7 +1,7 @@
-let sustainWidth = $('.sustain .s3_title').width(),
-    sustainContentWidth = $('.sustain .s3_content').width();
+// let sustainWidth = $('.sustain .s3_title').width(),
+//     sustainContentWidth = $('.sustain .s3_content').width();
 
-$('.sustain .s3_content').css({marginLeft: `${sustainWidth - sustainContentWidth}px`});
+// $('.sustain .s3_content').css({marginLeft: `${sustainWidth - sustainContentWidth}px`});
 
 // $('.tlt1').textillate({
 //     in: {
@@ -28,29 +28,79 @@ $(window).scroll(function(){
 });
 */
 
-$('.s3_title').lettering();
-$('.s3_content').lettering();
-$('.value_title').lettering();
-$('.value_content').lettering();
-$('.value_list_title').lettering();
-$('.value_list_content p').lettering();
+// $('.s3_title').lettering();
+// $('.s3_content').lettering();
+// $('.value_title').lettering();
+// $('.value_content').lettering();
+// $('.value_list_title').lettering();
+// $('.value_list_content p').lettering();
 
-    $(window).scroll(function(){
-        scrollFill($('.s3_title span'), 'white')
-        scrollFill($('.s3_content span'), 'white')
-        scrollFill($('.value_title span'), '#121212')
-        scrollFill($('.value_content span'), '#121212')
-        scrollFill($('.value_list_title span'), '#121212')
-        scrollFill($('.value_list_content p span'), '#121212')
-    })
+    // $(window).scroll(function(){
+    //     scrollFill($('.s3_title span'), 'white')
+    //     scrollFill($('.s3_content span'), 'white')
+    //     scrollFill($('.value_title span'), '#121212')
+    //     scrollFill($('.value_content span'), '#121212')
+    //     scrollFill($('.value_list_title span'), '#121212')
+    //     scrollFill($('.value_list_content p span'), '#121212')
+    // })
 
-    function scrollFill(scrVar, scrColor) {
-        let scrollAmt = $(window).scrollTop() - scrVar.offset().top,
-        currentIdx = Math.floor(scrollAmt / 20);
+    // function scrollFill(scrVar, scrColor) {
+    //     let scrollAmt = $(window).scrollTop() - scrVar.offset().top,
+    //     currentIdx = Math.floor(scrollAmt / 20);
   
-        scrVar.each(function(idx) {
-          if(idx === currentIdx){
-              $(this).css({color: scrColor})
-          }
-        });
-      }
+    //     scrVar.each(function(idx) {
+    //       if(idx === currentIdx){
+    //           $(this).css({color: scrColor})
+    //       }
+    //     });
+    //   }
+
+    let target = $('.target');
+    $('.content > *').lettering();
+    
+    
+    // console.log(letters);
+    
+    let lastScrollTop = 0;
+    let scrollCount = 0;
+    let isScrolling = false;
+      
+      $(window).scroll(function() {
+        let currentScrollTop = $(this).scrollTop();    
+    
+        if (currentScrollTop > lastScrollTop) {
+    
+            target.each(function(){
+              let targetSection = $(this);
+              if(targetSection.offset().top <= currentScrollTop){
+                if(!targetSection.hasClass('active')){
+                  if (!isScrolling) {      
+                          
+                    let letters = targetSection.find('span');
+                    if(scrollCount < letters.length){
+                      letters.eq(scrollCount++).addClass('active');
+                      isScrolling = true;
+                
+                      // 500ms의 간격으로 스크롤 이벤트 체크
+                      setTimeout(function() {
+                        isScrolling = false;
+                      }, 100);
+        
+                      if(scrollCount ===letters.length){
+                        scrollCount = 0;
+                        targetSection.addClass('active');
+                      }
+                    }              
+                    console.log(scrollCount);
+                  }
+                }else{
+                    $('.target').css({position: 'relative'})
+                    $('.blank').css({height: 0})
+                    $('html').animate({scrollTop: $('.value article').offset().top}, 400)
+                }
+              }        
+            });
+        } 
+        
+        lastScrollTop = currentScrollTop; // 이전 스크롤 위치 갱신
+      });
