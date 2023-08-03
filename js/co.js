82e442cef708d13ea1c5957c9de692cf84506a8f
stars2302 <stars2302@naver.com>
2023-08-03 16:08:02 +0900

    [Add] 코드추가

--- js/co.js
+++ js/co.js
@@ -1,20 +1,20 @@
-let co_imageH = $('.co_image').outerHeight(); // 2460 <- 1040 + 1420
-let co_imageOST = $('.co_image').offset().top; // 2891.703125
-let co_imageConH = $('.co_image .content').outerHeight();// 1040
+let co_imageH = $('.co_image').outerHeight(); // 4291 <- 1040 + 3251
+let co_imageOST = $('.co_image').offset().top; // 3016.296875
+let co_imageConH = $('.co_image .content').outerHeight();// 791
 
-
+console.log(co_imageConH);
 $(window).scroll(function(){
   //2900(시작) 4120(끝) -> 차이 1220
   //200의 출처는.....?
   let scroll = $(window).scrollTop() - co_imageOST + 200;
   let imageWidth = 0.5;
-  let mathScroll = scroll/(co_imageH-co_imageConH) * 100/20;
+  let mathScroll = scroll/(co_imageH-co_imageConH) * 100/200;
   
   if(imageWidth == 1){
     imageWidth = 1;
   } else {
     imageWidth += mathScroll;
   }
-  console.log(imageWidth)
+  console.log(mathScroll)
   $('.co_image img').css({transform:`scale(${imageWidth})`});
 });
