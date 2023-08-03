36769ecef03740d09954c630ae97c261be60b9f8
rltjdms <euni48730@gmail.com>
2023-08-03 23:29:48 +0900

    [Fix] Js 수정

--- js/main.js
+++ js/main.js
@@ -1,23 +1,37 @@
 // Main_section2_tech 시작
-const techwrapperSwiper = new Swiper(".tech-wrapper-slide", {
+let techwrapperSwiper = new Swiper(".tech2-slide", {
     // Optional parameters
     direction: "horizontal",
     loop: true,
-
+    slidesPerView: 1,
     // If we need pagination
     pagination: {
         el: ".swiper-pagination",
+        clickable: true,
     },
-
+  
     // Navigation arrows
     navigation: {
         nextEl: "#tech_tab .slide_btn.prev",
         prevEl: "#tech_tab .slide_btn.next",
     },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: ".swiper-scrollbar",
-    },
-});
-// Main_section2_tech 종료
+  
+  });
+  
+  let techMenu = $(".section2 .tec_tab_list > a"); //탭메뉴
+  techMenu.on("click", function(e) {
+    e.preventDefault();
+    let techBarTop = $(this).position().top;
+    //let teBox = techContent.index();
+    techwrapperSwiper.slideTo($(this).index());
+    let techBar = $(".section2 .tech_bar");
+    //  let techTab = $(".section2 .tec_tab");
+  
+    techBar.css({ top: techBarTop });
+    //console.log(techOST);
+  
+    $(this).addClass('active').siblings().removeClass('active')
+  
+    //techwrapperSwiper.slideTo(idx); 
+  
+  });
