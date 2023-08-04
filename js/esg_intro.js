64b96519951439d7c940a470a72033083ab67f4b
hoon95 <gmline_@naver.com>
2023-08-04 18:25:16 +0900

    [Add] esg 개요 초안 마무리

--- js/esg_intro.js
+++ js/esg_intro.js
@@ -55,52 +55,72 @@ $(window).scroll(function(){
     //     });
     //   }
 
-    let target = $('.target');
-    $('.content > *').lettering();
-    
-    
-    // console.log(letters);
-    
-    let lastScrollTop = 0;
-    let scrollCount = 0;
-    let isScrolling = false;
+$('.content > *').lettering();
+
+let target = $('.s3');
+let scrollCount = 0;
+let isScrolling = false;
+  
+  $(window).scroll(function() {
+    let targetSection = $('.s3');
+    if(targetSection.offset().top <= $(window).scrollTop()){
+      if(!targetSection.hasClass('active')){
+        if (!isScrolling) {
+          let letters = targetSection.find('span');
+          if(scrollCount < letters.length){
+            letters.eq(scrollCount++).addClass('active');
+            isScrolling = true;
       
-      $(window).scroll(function() {
-        let currentScrollTop = $(this).scrollTop();    
-    
-        if (currentScrollTop > lastScrollTop) {
-    
-            target.each(function(){
-              let targetSection = $(this);
-              if(targetSection.offset().top <= currentScrollTop){
-                if(!targetSection.hasClass('active')){
-                  if (!isScrolling) {      
-                          
-                    let letters = targetSection.find('span');
-                    if(scrollCount < letters.length){
-                      letters.eq(scrollCount++).addClass('active');
-                      isScrolling = true;
-                
-                      // 500ms의 간격으로 스크롤 이벤트 체크
-                      setTimeout(function() {
-                        isScrolling = false;
-                      }, 100);
-        
-                      if(scrollCount ===letters.length){
-                        scrollCount = 0;
-                        targetSection.addClass('active');
-                      }
-                    }              
-                    console.log(scrollCount);
-                  }
-                }else{
-                    $('.target').css({position: 'relative'})
-                    $('.blank').css({height: 0})
-                    $('html').animate({scrollTop: $('.value article').offset().top}, 400)
-                }
-              }        
-            });
-        } 
-        
-        lastScrollTop = currentScrollTop; // 이전 스크롤 위치 갱신
-      });
+            // 500ms의 간격으로 스크롤 이벤트 체크
+            setTimeout(function() {
+              isScrolling = false;
+            }, 100);
+
+            if(scrollCount === letters.length){
+              scrollCount = 0;
+              targetSection.addClass('active');
+              $(window).off('scroll');
+              scrollToNextSection();
+            }
+          }
+        }
+      }
+    }
+    // if(targetSection.hasClass('active')){
+    //   let valueHeight = $('.value').offset().top;
+    //   targetSection.css({height:'100vh', position: 'relative'});
+    //   $(window).scrollTop(valueHeight);
+    //   $(window).off('scroll');
+    // }
+    function scrollToNextSection() {
+      if (targetSection.next().length > 0) {
+        let nextSection = targetSection.next();
+        $('html, body').animate({ scrollTop: nextSection.offset().top }, 500, function() {
+        });
+      }
+    }
+  });
+
+
+  // swiper
+  var swiper = new Swiper('.value_slide_container', {
+    spaceBetween: 0,
+    centeredSlides: true,
+    speed: 4000,
+    autoplay: {
+      delay: 1
+    },
+    loop: true,
+    loopedSlides: 1,
+    slidesPerView: 'auto',
+    disableOnInteraction: true,
+    initialSlide: 3
+  });
+
+  let slideHeight = $('.value_slide_container').outerHeight();
+  $(window).resize(function(){
+    $('.value_bottom').css({height: slideHeight})
+  });
+  $(window).trigger('resize');
+
+  AOS.init();
