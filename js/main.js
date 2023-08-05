71b9c3df73802390dd7b0f4455ebdbfd09980d06
hoon95 <gmline_@naver.com>
2023-08-06 01:52:55 +0900

    [Add] main esg 내용 추가

--- js/main.js
+++ js/main.js
@@ -57,6 +57,42 @@ function activeteb(idx) {
 
 // Main_section2_tech 종료
 
+// Main_section3_esg
+$('.esg_hover').css({display: 'none'})
+$('.esg_list_exp').hover(
+    function(){
+        esgNumber();
+        $(this).stop().animate({width: '52vw'});
+        $(this).find('.esg_hover').css({display:'inline-block'});
+        $('.esg_exp').stop().animate({width: '13vw'})
+        $('.esg_title').css({writingMode: 'vertical-lr'})
+        $('.esg_desc').css({display: 'none'});
+    },
+    function(){
+        $(this).stop().animate({width: '17.3vw'})
+        $(this).find('.esg_hover').css({display:'none'});
+        $('.esg_exp').stop().animate({width: '47vw'});
+        $('.esg_title').css({writingMode: ''})
+        $('.esg_desc').css({display: 'block'});
+    }
+)
+
+let esgNumber = function(){
+    let esgHover = $('.esg_hover > div');
+    esgHover.each(function(){
+        let changeNum = $(this).find('span')
+        let targetNum = changeNum.attr('data-num');
+    
+        $({num:0}).animate({num:targetNum},{
+            duration: 2500,
+            progress: function(){
+                let change = Math.ceil(this.num)
+                changeNum.text(change);
+            }
+        })
+    })
+}
+
 
 // Main_section4_tech 시작
 let sec4_itemContainer = $('.section4 .slide_wrap');
