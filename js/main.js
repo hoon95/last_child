bee2a7b110b39ef96215ed5eea4f83e04dec2c10
stars2302 <stars2302@naver.com>
2023-08-05 19:18:24 +0900

    [Add] 메인 section4 스크립트 기능 추가

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // Main_section2_tech 시작
+/*
 const techwrapperSwiper = new Swiper(".tech-wrapper-slide", {
     // Optional parameters
     direction: "horizontal",
@@ -20,4 +21,26 @@ const techwrapperSwiper = new Swiper(".tech-wrapper-slide", {
         el: ".swiper-scrollbar",
     },
 });
+*/
 // Main_section2_tech 종료
+
+
+// Main_section4_tech 시작
+let sec4_itemContainer = $('.section4 .slide_wrap');
+let sec4_item = $('.section4 .slide_wrap li');
+let sec4OST = $('.section4').offset().top;
+
+
+$(window).scroll(function(){
+  let SCT = $(this).scrollTop() - sec4OST;
+  sec4_item.each(function(){
+    let $this = $(this);
+    let itemPosTop = $(this).position().top-500;
+    if(SCT > itemPosTop){
+      $this.addClass('active');
+    }
+  });
+  console.log(SCT);
+});
+$(window).trigger('scroll');
+// Main_section4_tech 종료
