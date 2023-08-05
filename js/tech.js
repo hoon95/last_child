2d7e5e69c8674a976083aaf4d1e00b9586be2f77
rltjdms <euni48730@gmail.com>
2023-08-06 02:45:29 +0900

    [FIx] css js qd.html

--- js/tech.js
+++ js/tech.js
@@ -1,13 +1,26 @@
 $(window).scroll(function() {
   let $this = $(this).scrollTop();
-  let qddotOST = $(".perspective").offset().top;
-  let isActive = false;
+  let qddotOST = $(".qd_dot").offset().top;
+  //let isActive = false;
   if($this >= qddotOST) {
-    if(!isActive) {
-      $(".qd_dot_ani > div").addClass("animate");
-      isActive = true;
-    } else {
+    $(".qd_dot_ani > div").addClass("animate");
+  }else {
       $(".qd_dot_ani > div").removeClass("animate");
-    }
+  }
+
+  let qdoledOST = $(".qd_oled_box").offset().top - 150;
+  if($this >= qdoledOST) {
+    $(".qd_oled_box").addClass("animate");
+  }else {
+      $(".qd_oled_box").removeClass("animate");
+  }
+
+  let qdrowOST = $(".qd.row").offset().top - 350;
+  if($this >= qdrowOST) {
+    $(".qd_oled_tv").addClass("animate");
+    $(".qd_display_od ").addClass("animate");
+  }else {
+      $(".qd_oled_tv").removeClass("animate");
+      $(".qd_display_od").removeClass("animate");
   }
 })
