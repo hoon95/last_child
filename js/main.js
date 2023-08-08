27ef0af27901c5b21a6e4b1a763f6ba825b2e9bc
hoon95 <gmline_@naver.com>
2023-08-08 16:15:28 +0900

    [Fix] main 타 section merge

--- js/main.js
+++ js/main.js
@@ -1,25 +1,60 @@
 // Main_section2_tech 시작
-// const techwrapperSwiper = new Swiper(".tech-wrapper-slide", {
-//     // Optional parameters
-//     direction: "horizontal",
-//     loop: true,
+let techwrapperSwiper; 
+techwrapperSwiper = new Swiper(".tech2-slide", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
 
-//     // If we need pagination
-//     pagination: {
-//         el: ".swiper-pagination",
-//     },
+    // Navigation arrows
+    navigation: {
+        nextEl: "#tech_tab .slide_btn.next",
+        prevEl: "#tech_tab .slide_btn.prev",
+    },
 
-//     // Navigation arrows
-//     navigation: {
-//         nextEl: "#tech_tab .slide_btn.prev",
-//         prevEl: "#tech_tab .slide_btn.next",
-//     },
+    //
+});
+techwrapperSwiper.on("slideChange", function () {
 
-//     // And if we need scrollbar
-//     scrollbar: {
-//         el: ".swiper-scrollbar",
-//     },
-// });
+
+   activeteb(techwrapperSwiper.realIndex);
+});
+
+let techMenu = $(".section2 .tec_tab_list > a"); //탭메뉴
+let techBar = $(".section2 .tech_bar");
+
+let techPrevBtn = $("#tech_tab .prev");
+let techNextBtn = $("#tech_tab .next");
+
+let tectebSlide = $(".tech2-slide .swiper-wrapper > div"); //탭 슬라이드
+
+let techMenuIdx = techMenu.length;
+let tectebIndex = tectebSlide.length;
+
+
+
+techMenu.on("click", function (e) {
+    e.preventDefault();
+    //let teBox = techContent.index();
+    let activeIndex = $(this).index();
+
+    activeteb(activeIndex);
+
+    techwrapperSwiper.slideTo(activeIndex);
+
+    $(this).addClass("active").siblings().removeClass("active");
+
+});
+
+function activeteb(idx) {
+    let techBarTop = techMenu.eq(idx).position().top;
+    techBar.css({ top: techBarTop });
+}
+
 // Main_section2_tech 종료
 
 
@@ -66,3 +101,24 @@ let esgNumber = function(){
 function numberFormat(inputNumber) {
    return inputNumber.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+
+// Main_section4_tech 시작
+let sec4_itemContainer = $('.section4 .slide_wrap');
+let sec4_item = $('.section4 .slide_wrap li');
+let sec4OST = $('.section4').offset().top;
+
+
+$(window).scroll(function(){
+  let SCT = $(this).scrollTop() - sec4OST;
+  sec4_item.each(function(){
+    let $this = $(this);
+    let itemPosTop = $(this).position().top-500;
+    if(SCT > itemPosTop){
+      $this.addClass('active');
+    }
+  });
+  console.log(SCT);
+});
+$(window).trigger('scroll');
+// Main_section4_tech 종료
